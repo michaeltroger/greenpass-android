5da7e15728c4db19cb0a2ad1140a46d41704436b
Michael Troger <dev@michaeltroger.com>
2021-06-22 20:18:16 +0200

    Version 1.1

--- app/src/main/java/com/michaeltroger/gruenerpass/MainActivity.kt
+++ app/src/main/java/com/michaeltroger/gruenerpass/MainActivity.kt
@@ -14,6 +14,10 @@ import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.io.File
 import java.io.FileOutputStream
 
@@ -21,15 +25,21 @@ private const val FILENAME = "certificate.pdf"
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var file: File
     private var bitmap: Bitmap? = null
-    private lateinit var certificateImage: ImageView
-    private lateinit var deleteButton: Button
+
+    private var certificateImage: ImageView? = null
+    private var addButton: Button? = null
+    private var deleteMenuItem: MenuItem? = null
 
     private var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
         if (result.resultCode == Activity.RESULT_OK) {
             result.data?.data?.also { uri ->
-                val file = copyFileToCache(uri)
-                renderPdf(ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY))
+                lifecycleScope.launch {
+                    copyPdfToCache(uri)
+                    parsePdfIntoBitmap()
+                    showCertificateState()
+                }
             }
         }
     }
@@ -39,31 +49,38 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.main_activity)
 
         certificateImage = findViewById(R.id.certificate)
-        deleteButton = findViewById(R.id.add)
+        addButton = findViewById(R.id.add)
 
-        val file = getFile()
-        if (file.exists()) {
-            renderPdf(ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY))
+        file = File(cacheDir, FILENAME)
+
+        lifecycleScope.launch {
+            if (file.exists()) {
+                parsePdfIntoBitmap()
+                showCertificateState()
+            } else {
+                showEmptyState()
+            }
         }
 
-        deleteButton.setOnClickListener {
+        addButton?.setOnClickListener {
             openFilePicker()
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu, menu)
+        deleteMenuItem = menu.findItem(R.id.delete)
+        deleteMenuItem?.isEnabled = file.exists()
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
         R.id.delete -> {
-            val file = getFile()
             if (file.exists()) {
                 file.delete()
+                bitmap = null
             }
-            certificateImage.setImageResource(0)
-            showDeleteButton()
+            showEmptyState()
             true
         }
         else -> super.onOptionsItemSelected(item)
@@ -77,35 +94,33 @@ class MainActivity : AppCompatActivity() {
         resultLauncher.launch(intent)
     }
 
-    private fun renderPdf(fileDescriptor: ParcelFileDescriptor) {
-        val renderer = PdfRenderer(fileDescriptor)
+    private fun showEmptyState() {
+        addButton?.isVisible = true
+        certificateImage?.setImageResource(0)
+        certificateImage?.isVisible = false
+        deleteMenuItem?.isEnabled = false
+    }
+
+    private fun showCertificateState() {
+        addButton?.isVisible = false
+        certificateImage?.setImageBitmap(bitmap)
+        certificateImage?.isVisible = true
+        deleteMenuItem?.isEnabled = true
+    }
+
+    private suspend fun parsePdfIntoBitmap() = withContext(Dispatchers.IO) {
+        val renderer = PdfRenderer(ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY))
         val page: PdfRenderer.Page = renderer.openPage(0)
 
         bitmap = Bitmap.createBitmap(page.width * 3, page.height * 3, Bitmap.Config.ARGB_8888)
         page.render(bitmap!!, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
-        certificateImage.setImageBitmap(bitmap)
 
         page.close()
         renderer.close()
-
-        showCertificate()
     }
 
-    private fun getFile() = File(cacheDir, FILENAME)
-
-    private fun showDeleteButton() {
-        deleteButton.isVisible = true
-        certificateImage.isVisible = false
-    }
-
-    private fun showCertificate() {
-        deleteButton.isVisible = false
-        certificateImage.isVisible = true
-    }
-
-    private fun copyFileToCache(uri: Uri): File {
+    private suspend fun copyPdfToCache(uri: Uri) = withContext(Dispatchers.IO) {
         val inputStream = contentResolver.openInputStream(uri)!!
-        val file = getFile()
 
         val output = FileOutputStream(file)
         val buffer = ByteArray(1024)
@@ -116,8 +131,6 @@ class MainActivity : AppCompatActivity() {
 
         inputStream.close()
         output.close()
-
-        return file
     }
 
 }
