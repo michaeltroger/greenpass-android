3e0237b03233595577b9a1442635a6df14adc19d
Michael Troger <dev@michaeltroger.com>
2021-06-27 12:50:30 +0200

    Extract qr code

--- app/src/main/java/com/michaeltroger/gruenerpass/MainActivity.kt
+++ app/src/main/java/com/michaeltroger/gruenerpass/MainActivity.kt
@@ -3,6 +3,7 @@ package com.michaeltroger.gruenerpass
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.Color
 import android.graphics.pdf.PdfRenderer
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
@@ -15,18 +16,29 @@ import android.widget.ImageView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
+import com.google.zxing.*
+import com.google.zxing.common.BitMatrix
+import com.google.zxing.qrcode.QRCodeReader
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.File
 import java.io.FileOutputStream
 
+import com.google.zxing.common.HybridBinarizer
+
+
 private const val FILENAME = "certificate.pdf"
+private const val QR_CODE_SIZE = 600
 
 class MainActivity : AppCompatActivity() {
 
+    private val qrCodeReader = QRCodeReader()
+    private val qrCodeWriter = MultiFormatWriter()
+
     private lateinit var file: File
-    private var bitmap: Bitmap? = null
+    private var bitmapDocument: Bitmap? = null
+    private var bitmapQrCode: Bitmap? = null
 
     private var certificateImage: ImageView? = null
     private var addButton: Button? = null
@@ -84,7 +96,7 @@ class MainActivity : AppCompatActivity() {
                 if (doesFileExist()) {
                     deleteFile()
                 }
-                bitmap = null
+                bitmapDocument = null
                 showEmptyState()
             }
             true
@@ -109,7 +121,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun showCertificateState() {
         addButton?.isVisible = false
-        certificateImage?.setImageBitmap(bitmap)
+        certificateImage?.setImageBitmap(bitmapQrCode)
         certificateImage?.isVisible = true
         deleteMenuItem?.isEnabled = true
     }
@@ -126,11 +138,48 @@ class MainActivity : AppCompatActivity() {
         val renderer = PdfRenderer(ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY))
         val page: PdfRenderer.Page = renderer.openPage(0)
 
-        bitmap = Bitmap.createBitmap(page.width * 3, page.height * 3, Bitmap.Config.ARGB_8888)
-        page.render(bitmap!!, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
+        bitmapDocument = Bitmap.createBitmap(page.width * 3, page.height * 3, Bitmap.Config.ARGB_8888)!!
+        page.render(bitmapDocument!!, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
 
         page.close()
         renderer.close()
+
+        extractQrCodeIfAvailable(bitmapDocument!!)
+    }
+
+    private fun extractQrCodeIfAvailable(bitmap: Bitmap) {
+        val intArray = IntArray(bitmap.width * bitmap.height)
+        bitmap.getPixels(intArray, 0, bitmap.width, 0, 0, bitmap.width, bitmap.height)
+        val source: LuminanceSource = RGBLuminanceSource(bitmap.width, bitmap.height, intArray)
+        val binaryBitmap = BinaryBitmap(HybridBinarizer(source))
+
+        try {
+            qrCodeReader.decode(binaryBitmap).text?.let {
+                encodeQrCodeAsBitmap(it)
+            }
+        } catch (ignore: Exception) {}
+    }
+
+    private fun encodeQrCodeAsBitmap(source: String) {
+        val result: BitMatrix = try {
+            qrCodeWriter.encode(source, BarcodeFormat.QR_CODE, QR_CODE_SIZE, QR_CODE_SIZE, null)
+        } catch (ignore: Exception) {
+            return
+        }
+
+        val w = result.width
+        val h = result.height
+        val pixels = IntArray(w * h)
+
+        for (y in 0 until h) {
+            val offset = y * w
+            for (x in 0 until w) {
+                pixels[offset + x] = if (result[x, y]) Color.BLACK else Color.WHITE
+            }
+        }
+
+        bitmapQrCode = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        bitmapQrCode!!.setPixels(pixels, 0, QR_CODE_SIZE, 0, 0, w, h)
     }
 
     private suspend fun copyPdfToCache(uri: Uri) = withContext(Dispatchers.IO) {
