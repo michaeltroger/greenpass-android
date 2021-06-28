206b268c24a882dc4c3c301c8f588c9765d4f136
Michael Troger <dev@michaeltroger.com>
2021-06-28 19:59:34 +0200

    Ignore exceptions during migration

--- app/src/main/java/com/michaeltroger/gruenerpass/update/AppMigrateFrom6.kt
+++ app/src/main/java/com/michaeltroger/gruenerpass/update/AppMigrateFrom6.kt
@@ -3,17 +3,19 @@ package com.michaeltroger.gruenerpass.update
 import android.content.Context
 import com.michaeltroger.gruenerpass.pdf.PDF_FILENAME
 import java.io.File
+import java.lang.Exception
 
 class AppMigrateFrom6 {
 
     operator fun invoke(context: Context) {
-        val src = File(context.cacheDir, "certificate.pdf")
+        try {
+            val src = File(context.cacheDir, "certificate.pdf")
 
-        if (src.exists()) {
-            val dest = File(context.filesDir, PDF_FILENAME)
-            src.copyTo(dest)
-            src.delete()
-        }
-
+            if (src.exists()) {
+                val dest = File(context.filesDir, PDF_FILENAME)
+                src.copyTo(dest)
+                src.delete()
+            }
+        } catch (ignore: Exception) {}
     }
 }
