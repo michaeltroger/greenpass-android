53d500bfd2ee8b2a78e8a2a17480d1b7794fb980
Michael Troger <dev@michaeltroger.com>
2021-07-08 22:53:24 +0200

    Refactoring

--- app/src/main/java/com/michaeltroger/gruenerpass/MainViewModel.kt
+++ app/src/main/java/com/michaeltroger/gruenerpass/MainViewModel.kt
@@ -5,20 +5,23 @@ import android.net.Uri
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import com.michaeltroger.gruenerpass.pdf.Pdf
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
 
 class MainViewModel(app: Application): AndroidViewModel(app) {
     val updatedUri = MutableSharedFlow<Uri>(extraBufferCapacity = 1)
-    val areBitmapsReady = MutableSharedFlow<Boolean>(extraBufferCapacity = 1)
 
-    val viewState = MutableStateFlow<ViewState>(ViewState.Empty)
-    val viewEvent = MutableSharedFlow<ViewEvent>(extraBufferCapacity = 1)
+    private val _areBitmapsReady = MutableSharedFlow<Boolean>(extraBufferCapacity = 1)
+    val areBitmapsReady: SharedFlow<Boolean> = _areBitmapsReady
 
-    private var runtimeUriReceived: Uri? = null
+    private val _viewState = MutableStateFlow<ViewState>(ViewState.Empty)
+    val viewState: StateFlow<ViewState> = _viewState
+
+    private val _viewEvent = MutableSharedFlow<ViewEvent>(extraBufferCapacity = 1)
+    val viewEvent: SharedFlow<ViewEvent> = _viewEvent
+
+    private var uriReceived: Uri? = null
 
     private val pdf = Pdf(getApplication<Application>())
 
@@ -26,21 +29,21 @@ class MainViewModel(app: Application): AndroidViewModel(app) {
         viewModelScope.launch {
             if (pdf.doesFileExist()) {
                 if (parsePdfIntoBitmap()) {
-                    viewState.emit(ViewState.Certificate)
+                    _viewState.emit(ViewState.Certificate)
                 } else {
-                    viewState.emit(ViewState.Error)
+                    _viewState.emit(ViewState.Error)
                 }
             } else {
-                viewState.emit(ViewState.Empty)
+                _viewState.emit(ViewState.Empty)
             }
         }
 
         viewModelScope.launch {
             updatedUri.collect {
-                runtimeUriReceived = it
-                viewEvent.emit(ViewEvent.CloseAllDialogs)
+                uriReceived = it
+                _viewEvent.emit(ViewEvent.CloseAllDialogs)
                 if (pdf.doesFileExist()) {
-                    viewEvent.emit(ViewEvent.ShowReplaceDialog)
+                    _viewEvent.emit(ViewEvent.ShowReplaceDialog)
                 } else {
                     handleFileFromUri(it)
                 }
@@ -51,13 +54,13 @@ class MainViewModel(app: Application): AndroidViewModel(app) {
     private fun handleFileFromUri(uri: Uri) {
         viewModelScope.launch {
             if (pdf.isPdfPasswordProtected(uri)) {
-                viewEvent.emit(ViewEvent.ShowPasswordDialog)
+                _viewEvent.emit(ViewEvent.ShowPasswordDialog)
             } else {
-                viewState.emit(ViewState.Empty)
+                _viewState.emit(ViewState.Empty)
                 if (pdf.copyPdfToCache(uri) && parsePdfIntoBitmap()) {
-                    viewState.emit(ViewState.Certificate)
+                    _viewState.emit(ViewState.Certificate)
                 } else {
-                    viewState.emit(ViewState.Error)
+                    _viewState.emit(ViewState.Error)
                 }
             }
         }
@@ -66,26 +69,26 @@ class MainViewModel(app: Application): AndroidViewModel(app) {
     private suspend fun parsePdfIntoBitmap(): Boolean {
         val success = pdf.parsePdfIntoBitmap()
         if (success) {
-            areBitmapsReady.emit(true)
+            _areBitmapsReady.emit(true)
         }
         return success
     }
 
     fun onReplaceConfirmed() {
-        handleFileFromUri(runtimeUriReceived!!)
+        handleFileFromUri(uriReceived!!)
     }
 
     fun onPasswordEntered(password: String) {
         viewModelScope.launch {
-            if (pdf.decryptAndCopyPdfToCache(uri = runtimeUriReceived!!, password = password)) {
-                viewState.emit(ViewState.Empty)
+            if (pdf.decryptAndCopyPdfToCache(uri = uriReceived!!, password = password)) {
+                _viewState.emit(ViewState.Empty)
                 if (parsePdfIntoBitmap()) {
-                    viewState.emit(ViewState.Certificate)
+                    _viewState.emit(ViewState.Certificate)
                 } else {
-                    viewState.emit(ViewState.Error)
+                    _viewState.emit(ViewState.Error)
                 }
             } else {
-                viewEvent.emit(ViewEvent.ShowPasswordDialog)
+                _viewEvent.emit(ViewEvent.ShowPasswordDialog)
             }
         }
     }
@@ -93,7 +96,7 @@ class MainViewModel(app: Application): AndroidViewModel(app) {
     fun onDeleteConfirmed() {
         viewModelScope.launch {
             pdf.deleteFile()
-            viewState.emit(ViewState.Empty)
+            _viewState.emit(ViewState.Empty)
         }
     }
 
