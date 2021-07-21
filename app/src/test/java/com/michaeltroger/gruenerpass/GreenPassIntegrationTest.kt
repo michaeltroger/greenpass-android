33d3a3101a37137bdf4a43344f974a1136e654f9
Michael Troger <dev@michaeltroger.com>
2021-07-22 00:23:38 +0200

    Check delete icon in test

--- app/src/test/java/com/michaeltroger/gruenerpass/GreenPassIntegrationTest.kt
+++ app/src/test/java/com/michaeltroger/gruenerpass/GreenPassIntegrationTest.kt
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -34,6 +35,7 @@ class GreenPassIntegrationTest {
             onView(withId(R.id.tab_layout)).check(matches(not(isDisplayed())))
             onView(withId(R.id.pager)).check(matches(not(isDisplayed())))
             onView(withId(R.id.progress_indicator)).check(matches(not(isDisplayed())))
+            onView(withId(R.id.delete)).check(doesNotExist())
         }
     }
 
@@ -48,6 +50,8 @@ class GreenPassIntegrationTest {
         scenario.onActivity {
             onView(withId(R.id.tab_layout)).check(matches(isDisplayed()))
             onView(withId(R.id.pager)).check(matches(isDisplayed()))
+            onView(withId(R.id.delete)).check(matches(isDisplayed()))
+
             onView(withId(R.id.qrcode)).check(matches(isDisplayed()))
             Assertions.assertThat(it.findViewById<ImageView>(R.id.qrcode).drawable).isNotNull
 
@@ -55,8 +59,8 @@ class GreenPassIntegrationTest {
             onView(withId(R.id.add)).check(matches(not(isDisplayed())))
 
             onView(withText(R.string.tab_title_pdf)).perform(click())
-            onView(withId(R.id.certificate)).check(matches(isDisplayed()))
 
+            onView(withId(R.id.certificate)).check(matches(isDisplayed()))
             Assertions.assertThat(it.findViewById<RecyclerView>(R.id.certificate).findViewById<ImageView>(R.id.page).drawable).isNotNull
         }
     }
