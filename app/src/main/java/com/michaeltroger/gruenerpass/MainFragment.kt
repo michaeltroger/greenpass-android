3fdcff9432499452d1a79561b10ca1f48a0910a5
Michael Troger <dev@michaeltroger.com>
2021-09-26 18:47:53 +0200

    Remove password login again

--- app/src/main/java/com/michaeltroger/gruenerpass/MainFragment.kt
+++ app/src/main/java/com/michaeltroger/gruenerpass/MainFragment.kt
@@ -110,9 +110,9 @@ class MainFragment : Fragment(R.layout.fragment_main) {
 
         promptInfo = BiometricPrompt.PromptInfo.Builder()
             .setTitle(requireContext().getString(R.string.biometric_login_title))
-            //.setNegativeButtonText(requireContext().getString(R.string.cancel))
+            .setNegativeButtonText(requireContext().getString(R.string.cancel))
             .setConfirmationRequired(false)
-            .setAllowedAuthenticators(BiometricManager.Authenticators.BIOMETRIC_WEAK or BiometricManager.Authenticators.DEVICE_CREDENTIAL)
+            .setAllowedAuthenticators(BiometricManager.Authenticators.BIOMETRIC_WEAK)
             .build()
 
         binding.authenticate.setOnClickListener {
