2d77019c85a0fa714ee487da530f23da3289d802
Michael Troger <dev@michaeltroger.com>
2022-11-05 16:59:36 +0100

    Update sdk and dependencies (#69)
    * Update sdk and dependencies
    
    * Update pdfbox

--- app/src/main/java/com/michaeltroger/gruenerpass/GreenPassApplication.kt
+++ app/src/main/java/com/michaeltroger/gruenerpass/GreenPassApplication.kt
@@ -2,13 +2,11 @@ package com.michaeltroger.gruenerpass
 
 import android.app.Application
 import com.michaeltroger.gruenerpass.update.AppMigrator
-import com.tom_roush.pdfbox.util.PDFBoxResourceLoader
 
 class GreenPassApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
         AppMigrator(applicationContext).performMigration()
-        PDFBoxResourceLoader.init(this)
     }
 }
