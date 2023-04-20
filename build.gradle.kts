459d2e31093eac16869936adb7a7192f3eaf7df3
Michael Troger <dev@michaeltroger.com>
2023-04-20 20:07:00 +0200

    Add licenses (#83)
    * Add licenses
    
    * Add to github CI

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,5 @@ plugins {
     alias(libs.plugins.com.android.application) apply false
     alias(libs.plugins.com.google.devtools.ksp) apply false
     alias(libs.plugins.org.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.com.github.jk1.dependency.license.report) apply false
 }
