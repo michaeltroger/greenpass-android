dc2440925b659aa4081db54532e22428aa2f661d
Michael Troger <dev@michaeltroger.com>
2023-04-20 21:48:41 +0200

    Add license and version to more (#87)
    * Add license to more
    
    * Display version
    
    * Remove unneeded spacing

--- app/src/main/java/com/michaeltroger/gruenerpass/more/MoreFragment.kt
+++ app/src/main/java/com/michaeltroger/gruenerpass/more/MoreFragment.kt
@@ -1,12 +1,17 @@
 package com.michaeltroger.gruenerpass.more
 
 import android.os.Bundle
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.michaeltroger.gruenerpass.R
+import com.michaeltroger.gruenerpass.extensions.getPackageInfo
 
 class MoreFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.more, rootKey)
+
+        val preference = findPreference<Preference>(getString(R.string.key_preference_version)) ?: error("Preference is required")
+        preference.title = getString(R.string.version, requireContext().getPackageInfo().versionName!!)
     }
 }
