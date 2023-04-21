10a593ca865df11ecfe48c43cdb8244f6ac6d908
Michael Troger <dev@michaeltroger.com>
2023-04-21 21:19:20 +0200

    Apply utils for code quality

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.com.android.application) apply false
-    alias(libs.plugins.com.google.devtools.ksp) apply false
-    alias(libs.plugins.org.jetbrains.kotlin.android) apply false
     alias(libs.plugins.com.github.jk1.dependency.license.report) apply false
+    alias(libs.plugins.com.google.devtools.ksp) apply false
+    alias(libs.plugins.io.gitlab.arturbosch.detekt) apply false
+    alias(libs.plugins.org.jetbrains.kotlin.android) apply false
 }
