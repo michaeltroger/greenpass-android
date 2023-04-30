753f3c975093f9b78afb5ff860a931d64d534de5
Michael Troger <dev@michaeltroger.com>
2023-04-30 22:40:20 +0200

    Automated updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,3 @@
-import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import com.github.jk1.license.filter.*
 import io.gitlab.arturbosch.detekt.Detekt
 import java.io.FileInputStream
@@ -7,12 +6,10 @@ import java.util.Properties
 
 plugins {
     alias(libs.plugins.com.android.application)
-    alias(libs.plugins.com.github.ben.manes.versions)
     alias(libs.plugins.com.github.jk1.dependency.license.report)
     alias(libs.plugins.com.google.devtools.ksp)
     alias(libs.plugins.io.gitlab.arturbosch.detekt)
     alias(libs.plugins.org.jetbrains.kotlin.android)
-    alias(libs.plugins.nl.littlerobots.version.catalog.update)
 }
 
 android {
@@ -94,19 +91,6 @@ licenseReport {
     outputDir = "$rootDir/docs/licenses"
     configurations = arrayOf("releaseRuntimeClasspath")
     filters = arrayOf(LicenseBundleNormalizer(), ExcludeTransitiveDependenciesFilter())
-}
-
-fun isNonStable(version: String): Boolean {
-    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
-    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
-    val isStable = stableKeyword || regex.matches(version)
-    return isStable.not()
-}
-
-tasks.withType<DependencyUpdatesTask> {
-    rejectVersionIf {
-        isNonStable(this.candidate.version) && !isNonStable(this.currentVersion)
-    }
 }
 
 dependencies {
