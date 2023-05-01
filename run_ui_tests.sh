c9964e624a9adf5c00225eb9c98a03221134f13a
Michael Troger <dev@michaeltroger.com>
2023-05-01 15:44:25 +0200

    Feature/improve ci (#110)
    * Update script
    
    * Improve script
    
    * Improve script
    
    * Add comment
    
    * Always upload screenshots
    
    * Add comment
    
    * Simplify robot
    
    * Improve robot
    
    * Remove workaround
    
    * Improve script
    
    * Fail on errors
    
    * Add files
    
    * Remove file

--- run_ui_tests.sh
+++ run_ui_tests.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-rm -rd screenshots | true
+set -e # fail on errors
+
+rm -rd screenshots
 
 adb root
 adb wait-for-device
@@ -9,7 +11,6 @@ adb shell rm -f -rR /sdcard/Pictures/screenshots
 adb shell rm -f -rR /sdcard/Pictures/error_screenshots
 
 adb shell mkdir /sdcard/testdata
-while [ $? -ne 0 ]; do !!; sleep 1; done
 adb push testdata /sdcard/
 
 adb shell settings put global sysui_demo_allowed 1
@@ -19,11 +20,16 @@ adb shell am broadcast -a com.android.systemui.demo -e command network -e mobile
 adb shell am broadcast -a com.android.systemui.demo -e command network -e wifi show -e level 4
 adb shell am broadcast -a com.android.systemui.demo -e command battery -e plugged false
 adb shell am broadcast -a com.android.systemui.demo -e command battery -e level 100
+# run a second time since this one seems to be flaky on the emulator:
+adb shell am broadcast -a com.android.systemui.demo -e command network -e mobile show -e datatype none -e level 4
+
+set +e # don't fail on errors
 
 ./gradlew connectedDebugAndroidTest
 exit_code=$?
 
-adb pull /sdcard/Pictures/error_screenshots app/build | true
-adb pull /sdcard/Pictures/screenshots | true
+adb pull /sdcard/Pictures/error_screenshots app/build
+set -e # fail on errors
+adb pull /sdcard/Pictures/screenshots
 
 exit $exit_code
