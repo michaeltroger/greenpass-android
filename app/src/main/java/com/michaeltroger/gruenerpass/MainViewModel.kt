6ba63535a96aaaf70f40b9235dd1a996859187db
Michael Troger <dev@michaeltroger.com>
2023-05-13 20:37:38 +0200

    Fix detekt

--- app/src/main/java/com/michaeltroger/gruenerpass/MainViewModel.kt
+++ app/src/main/java/com/michaeltroger/gruenerpass/MainViewModel.kt
@@ -131,7 +131,7 @@ class MainViewModel(
         }
     }
 
-    @Suppress("TooGenericExceptionCaught")
+    @Suppress("TooGenericExceptionCaught, SpreadOperator")
     private suspend fun insertIntoDatabaseIfValidPdf() {
         val pendingFile = pendingFile!!
         val renderer = PdfRendererBuilder.create(
