39213cb3f20846c35cd841d14e2bbf14124719a3
Michael Troger <dev@michaeltroger.com>
2023-10-30 21:03:16 +0100

    Fix pdf export

--- app/src/main/java/com/michaeltroger/gruenerpass/file/PdfFileProvider.kt
+++
@@ -1,6 +0,0 @@
-package com.michaeltroger.gruenerpass.file
-
-import androidx.core.content.FileProvider
-import com.michaeltroger.gruenerpass.R
-
-class PdfFileProvider : FileProvider(R.xml.file_paths)
