d8f95166ed196ee4c319642a2dbbaaa2c3911761
Michael Troger <dev@michaeltroger.com>
2023-11-03 00:11:45 +0100

    Show installer in More

--- app/src/main/java/com/michaeltroger/gruenerpass/more/MoreFragment.kt
+++ app/src/main/java/com/michaeltroger/gruenerpass/more/MoreFragment.kt
@@ -1,5 +1,6 @@
 package com.michaeltroger.gruenerpass.more
 
+import android.os.Build
 import android.os.Bundle
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
@@ -11,9 +12,29 @@ class MoreFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.more, rootKey)
 
+        setVersionAndInstaller()
+    }
+
+    private fun setVersionAndInstaller() {
         val preference = findPreference<Preference>(
             getString(R.string.key_preference_version)
         ) ?: error("Preference is required")
+
         preference.title = getString(R.string.version, requireContext().getPackageInfo().versionName!!)
+
+        val installerPackageName =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                requireContext().packageManager.getInstallSourceInfo(requireContext().packageName).installingPackageName
+            } else {
+                @Suppress("DEPRECATION")
+                requireContext().packageManager.getInstallerPackageName(requireContext().packageName)
+            }
+        preference.summary = when (installerPackageName) {
+            "com.android.vending" -> "Google Play Store"
+            "com.amazon.venezia" -> "Amazon Appstore"
+            "com.huawei.appmarket" -> "Huawei AppGallery"
+            "org.fdroid.fdroid" -> "F-Droid"
+            else -> null
+        }
     }
 }
