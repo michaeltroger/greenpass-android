6eebf2b81bbe34e37c3e89b0e2c4146e994de911
Michael Troger <dev@michaeltroger.com>
2023-11-19 23:01:35 +0100

    Encrypted shared prefs (#174)
    * Use encrypted shared prefs
    
    * Add migration
    
    * Bump version
    
    * Cleanup
    
    * Disable backup of sharedprefs
    
    * Assure execution is performed
    
    * Add line break

--- app/src/main/java/com/michaeltroger/gruenerpass/locator/Locator.kt
+++ app/src/main/java/com/michaeltroger/gruenerpass/locator/Locator.kt
@@ -1,8 +1,12 @@
 package com.michaeltroger.gruenerpass.locator
 
 import android.content.Context
+import android.content.SharedPreferences
 import androidx.biometric.BiometricPrompt
 import androidx.room.Room
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+import com.michaeltroger.gruenerpass.GreenPassApplication
 import com.michaeltroger.gruenerpass.R
 import com.michaeltroger.gruenerpass.db.AppDatabase
 import com.michaeltroger.gruenerpass.dialogs.CertificateDialogs
@@ -17,6 +21,7 @@ import com.michaeltroger.gruenerpass.pdf.PdfDecryptor
 import com.michaeltroger.gruenerpass.pdf.PdfDecryptorImpl
 import com.michaeltroger.gruenerpass.qr.QrRenderer
 import com.michaeltroger.gruenerpass.qr.QrRendererImpl
+import com.michaeltroger.gruenerpass.settings.EncryptedPreferenceDataStore
 import com.michaeltroger.gruenerpass.settings.PreferenceObserver
 import com.michaeltroger.gruenerpass.settings.PreferenceObserverImpl
 import com.michaeltroger.gruenerpass.settings.SettingsFragment
@@ -25,6 +30,9 @@ import com.michaeltroger.gruenerpass.sharing.PdfSharingImpl
 
 object Locator {
 
+    val context: Context by lazy {
+        GreenPassApplication.instance
+    }
     fun database(context: Context): AppDatabase = Room.databaseBuilder(
         context.applicationContext,
         AppDatabase::class.java, "greenpass"
@@ -54,4 +62,22 @@ object Locator {
     fun pdfSharing(): PdfSharing = PdfSharingImpl()
 
     fun certificateDialogs(): CertificateDialogs = CertificateDialogsImpl()
+
+    val encryptedSharedPreferences: SharedPreferences by lazy {
+        EncryptedSharedPreferences.create(
+            context,
+            "secret_shared_prefs",
+            MasterKey.Builder(context)
+                .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+                .build(),
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+    }
+
+    val encryptedPreferenceDataStore: EncryptedPreferenceDataStore by lazy {
+        EncryptedPreferenceDataStore(
+            encryptedSharedPreferences
+        )
+    }
 }
