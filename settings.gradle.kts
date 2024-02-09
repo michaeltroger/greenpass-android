410f3c3db2528ae1e869e62aeb90e278bd695763
Michael Troger <dev@michaeltroger.com>
2024-02-09 18:58:50 +0100

    Try auto resolver

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,5 +13,8 @@ dependencyResolutionManagement {
         maven { setUrl("https://jitpack.io") }
     }
 }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
 
 include(":app")
