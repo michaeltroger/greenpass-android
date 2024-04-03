7648fa2377c15b5de806f7ade0d1e7e7c22698fc
Michael Troger <11340859+michaeltroger@users.noreply.github.com>
2024-04-03 22:05:03 +0200

    Fix sorting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,8 +6,8 @@ import java.util.Properties
 plugins {
     alias(libs.plugins.com.android.application)
     alias(libs.plugins.com.github.jk1.dependency.license.report)
-    alias(libs.plugins.com.google.devtools.ksp)
     alias(libs.plugins.com.google.dagger.hilt.android)
+    alias(libs.plugins.com.google.devtools.ksp)
     alias(libs.plugins.io.gitlab.arturbosch.detekt)
     alias(libs.plugins.org.jetbrains.kotlin.android)
     alias(libs.plugins.org.jetbrains.kotlin.plugin.parcelize)
