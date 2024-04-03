0406f24bd3d0b20e302a980a0e40839a47b5a79f
Michael Troger <11340859+michaeltroger@users.noreply.github.com>
2024-04-03 22:21:42 +0200

    Cleanup unused imports

--- app/src/main/java/com/michaeltroger/gruenerpass/dialogs/di/DialogsModule.kt
+++ app/src/main/java/com/michaeltroger/gruenerpass/dialogs/di/DialogsModule.kt
@@ -3,8 +3,6 @@ package com.michaeltroger.gruenerpass.dialogs.di;
 
 import com.michaeltroger.gruenerpass.dialogs.CertificateDialogs
 import com.michaeltroger.gruenerpass.dialogs.CertificateDialogsImpl
-import com.michaeltroger.gruenerpass.sharing.PdfSharing
-import com.michaeltroger.gruenerpass.sharing.PdfSharingImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
