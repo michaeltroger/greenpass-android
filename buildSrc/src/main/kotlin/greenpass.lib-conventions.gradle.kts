796ba94be93fe769cb678ac78273199120f70631
Michael Troger <11340859+michaeltroger@users.noreply.github.com>
2024-04-15 19:23:23 +0200

    Add detekt to libs

--- buildSrc/src/main/kotlin/greenpass.lib-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/greenpass.lib-conventions.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
     id("com.google.dagger.hilt.android")
+    
+    id("io.gitlab.arturbosch.detekt")
 }
 
 android {
