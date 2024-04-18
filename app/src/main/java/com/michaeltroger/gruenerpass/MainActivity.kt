395d6bde0276f54c7ed4684bcef912b6575c941f
Michael Troger <11340859+michaeltroger@users.noreply.github.com>
2024-04-18 03:05:55 +0200

    Remove outdated code

--- app/src/main/java/com/michaeltroger/gruenerpass/MainActivity.kt
+++ app/src/main/java/com/michaeltroger/gruenerpass/MainActivity.kt
@@ -1,14 +1,9 @@
 package com.michaeltroger.gruenerpass
 
 import android.content.Intent
-import android.graphics.Rect
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.view.MotionEvent
-import android.view.View
-import android.view.inputmethod.InputMethodManager
-import android.widget.EditText
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -114,25 +109,6 @@ class MainActivity : AppCompatActivity(R.layout.activity_main), AddFile {
 
     override fun addFile() {
         documentPick.launch(arrayOf(PDF_MIME_TYPE))
-    }
-
-    /**
-     * To remove focus from any EditText when click outside
-     */
-    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
-        if (event.action == MotionEvent.ACTION_DOWN) {
-            val v: View? = currentFocus
-            if (v is EditText) {
-                val outRect = Rect()
-                v.getGlobalVisibleRect(outRect)
-                if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
-                    v.clearFocus()
-                    val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
-                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0)
-                }
-            }
-        }
-        return super.dispatchTouchEvent(event)
     }
 
     override fun onUserInteraction() {
